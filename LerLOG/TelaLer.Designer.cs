1d41ffa30909273ef0f65c6c9815382543fc4d3a
JEFFERSON <97604882+jeffbrandao@users.noreply.github.com>
2022-06-03 11:25:48 -0400

    commit

---
+++ LerLOG/TelaLer.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace LerLOG
+{
+    partial class TelaLerLog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaLerLog));
+            this.gridLog = new System.Windows.Forms.DataGridView();
+            this.lbAviso = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.timerCount = new System.Windows.Forms.Timer(this.components);
+            this.lbtime = new System.Windows.Forms.Label();
+            this.btParar = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lbRodape = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridLog)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridLog
+            // 
+            this.gridLog.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridLog.BackgroundColor = System.Drawing.Color.White;
+            this.gridLog.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.gridLog.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            this.gridLog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridLog.Location = new System.Drawing.Point(0, 52);
+            this.gridLog.Name = "gridLog";
+            this.gridLog.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.gridLog.RowHeadersVisible = false;
+            this.gridLog.Size = new System.Drawing.Size(473, 439);
+            this.gridLog.TabIndex = 0;
+            // 
+            // lbAviso
+            // 
+            this.lbAviso.AutoSize = true;
+            this.lbAviso.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAviso.ForeColor = System.Drawing.Color.Red;
+            this.lbAviso.Location = new System.Drawing.Point(-2, 2);
+            this.lbAviso.Name = "lbAviso";
+            this.lbAviso.Size = new System.Drawing.Size(79, 13);
+            this.lbAviso.TabIndex = 9;
+            this.lbAviso.Text = "mensagem erro";
+            this.lbAviso.Visible = false;
+            // 
+            // timerCount
+            // 
+            this.timerCount.Interval = 1000;
+            this.timerCount.Tick += new System.EventHandler(this.timerCount_Tick);
+            // 
+            // lbtime
+            // 
+            this.lbtime.AutoSize = true;
+            this.lbtime.BackColor = System.Drawing.Color.Transparent;
+            this.lbtime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbtime.Location = new System.Drawing.Point(333, 29);
+            this.lbtime.Name = "lbtime";
+            this.lbtime.Size = new System.Drawing.Size(27, 17);
+            this.lbtime.TabIndex = 19;
+            this.lbtime.Text = "0:s";
+            // 
+            // btParar
+            // 
+            this.btParar.BackColor = System.Drawing.Color.White;
+            this.btParar.Image = ((System.Drawing.Image)(resources.GetObject("btParar.Image")));
+            this.btParar.Location = new System.Drawing.Point(425, 2);
+            this.btParar.Name = "btParar";
+            this.btParar.Size = new System.Drawing.Size(48, 44);
+            this.btParar.TabIndex = 11;
+            this.btParar.UseVisualStyleBackColor = false;
+            this.btParar.Click += new System.EventHandler(this.btParar_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.BackColor = System.Drawing.Color.White;
+            this.btnRefresh.Image = ((System.Drawing.Image)(resources.GetObject("btnRefresh.Image")));
+            this.btnRefresh.Location = new System.Drawing.Point(371, 2);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(48, 44);
+            this.btnRefresh.TabIndex = 10;
+            this.btnRefresh.UseVisualStyleBackColor = false;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // lbRodape
+            // 
+            this.lbRodape.AutoSize = true;
+            this.lbRodape.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbRodape.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.lbRodape.Location = new System.Drawing.Point(-1, 494);
+            this.lbRodape.Name = "lbRodape";
+            this.lbRodape.Size = new System.Drawing.Size(46, 13);
+            this.lbRodape.TabIndex = 20;
+            this.lbRodape.Text = "rodape";
+            // 
+            // TelaLerLog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(473, 510);
+            this.Controls.Add(this.lbRodape);
+            this.Controls.Add(this.lbtime);
+            this.Controls.Add(this.btParar);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.lbAviso);
+            this.Controls.Add(this.gridLog);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "TelaLerLog";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ler LOG - V.1.0.0.2";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.gridLog)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridLog;
+        private System.Windows.Forms.Label lbAviso;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Timer timerCount;
+        private System.Windows.Forms.Button btParar;
+        private System.Windows.Forms.Label lbtime;
+        private System.Windows.Forms.Label lbRodape;
+    }
+}
+
---
+++ LerLOG/TelaLer.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+// ===============================
+// AUTHOR       : JEFFERSON BRANDÃO DA COSTA - ANALISTA/PROGRAMADOR
+// CREATE DATE  : 14/05/2018
+// DESCRIPTION  : Sistema para ler pasta LOG das estaçoes  ASSY, FAT, TPT modelo E331
+// SPECIAL NOTES:
+// ===============================
+// Change History: version 1.0.0.2
+// Date:11/06/18 Correção para não travar programa interface qdo programa lerLog acessar arquivo [Abrir arquivo com permissao somente leitura]  
+//==================================
+
+namespace LerLOG
+{
+    public partial class TelaLerLog : Form
+    {
+        int statusConexao = 0;
+        int segundos = 0;
+        int tempo = 0;
+
+        public TelaLerLog()
+        {
+            InitializeComponent();
+            //  
+            tempo = GetTempo();
+            //
+            toolTip1.SetToolTip(btnRefresh, "Ligar");
+            toolTip1.SetToolTip(btParar, "Parar");
+            //
+            #region RODAPÉ
+
+            int anoCriacao = 2018;
+            int anoAtual = DateTime.Now.Year;
+            string texto = anoCriacao == anoAtual ? " Foxconn CNSBG All Rights Reserved." : "-" + anoAtual + " Foxconn CNSBG All Rights Reserved.";
+            //
+            lbRodape.Text = "Copyright © " + anoCriacao + texto;
+
+            #endregion
+            //
+            Process aProcess = Process.GetCurrentProcess();
+            string aProcName = aProcess.ProcessName;
+
+            if (Process.GetProcessesByName(aProcName).Length > 1)
+            {
+                MessageBox.Show("O programa já está em execução!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //
+            timerCount.Start();
+            statusConexao = 1;
+        }
+
+        public void GetArquivo()
+        {
+            #region LOG txt
+
+            string nomeArquivo = DateTime.Now.Date.ToString("yyyy-M-dd") + ".txt";
+            string caminho = GetCaminho();
+            string arquivo = caminho + nomeArquivo;
+            string linha;
+            string log = string.Empty;
+            string nomeColuna = string.Empty;
+            //
+            gridLog.Rows.Clear();
+
+            nomeColuna = caminho.Replace(@"\", string.Empty).Replace("EFAPWEB-SERVER", string.Empty).Trim(); //\\EFAPWEB-SERVER\LogFAT\
+            nomeColuna = string.IsNullOrEmpty(nomeColuna) ? "LOG" : nomeColuna;
+            //
+            if (gridLog.ColumnCount == 0)//Para refresh não duplicar coluna
+            {
+                gridLog.Columns.Add(nomeColuna, nomeColuna);//CRIA COLUNA
+            }
+            //
+            if (System.IO.File.Exists(arquivo))
+            {
+                #region CONFIGURAR ARQUIVO PARA SOMENTE LEITURA
+
+                //FileStream d = new FileStream(arquivo.Trim(), FileMode.OpenOrCreate, FileAccess.Write);
+                //StreamWriter c = new StreamWriter(d);
+                //c.Write(false);//abrir arquivo permissao somente leitura para não travar programa INTERFACE
+                //c.Close();
+
+                FileStream d = new FileStream(arquivo.Trim(), FileMode.OpenOrCreate, FileAccess.Read);
+
+               
+
+                #endregion
+
+                System.IO.StreamReader arqTXT = new System.IO.StreamReader(arquivo);
+
+                //gridLog.Columns.Add("LOG", "LOG");//CRIA COLUNA
+                while ((linha = arqTXT.ReadLine()) != null)
+                {
+                    if (!linha.Contains("File not found"))
+                        gridLog.Rows.Add(linha);
+                }
+
+                d.Flush();
+                d.Close();
+
+                //Ordem decrescente
+                gridLog.Sort(gridLog.Columns[0], ListSortDirection.Descending);
+                gridLog.Columns[0].HeaderCell.SortGlyphDirection = System.Windows.Forms.SortOrder.Descending;
+                //
+                gridLog.Rows[0].Selected = true;//deixar selecionado o item mais recente da lista
+                //
+                arqTXT.Close();
+                //
+                lbAviso.Visible = false;
+            }
+            else
+            {
+                lbAviso.Visible = true;
+                lbAviso.Text = "Arquivo ou caminho não existe " + arquivo;
+            }
+            //
+            if (gridLog.Rows.Count > 0)
+            {
+                for (int rows = 0; rows < gridLog.Rows.Count; rows++)
+                {
+                    //aumenta tamanho das linhas
+                    DataGridViewRow row = gridLog.Rows[rows];
+                    row.Height = 50;
+
+                    //deixa as linhas em negrito
+                    DataGridViewCellStyle style = gridLog.RowsDefaultCellStyle;
+                    style.Font = new Font(gridLog.Font, FontStyle.Bold);
+
+                    if (rows > 0)//nao permite deixar selecinado nenhum item que nao seja o mais recente da lista
+                        gridLog.Rows[rows].Selected = false;
+                }
+            }
+
+            #endregion
+        }
+
+        public string GetCaminho()
+        {
+            #region CAMINHO txt
+
+            string arquivo = AppDomain.CurrentDomain.BaseDirectory + @"\CONFIG\CAMINHO.txt";
+            string linha;
+            string caminho = string.Empty;
+            //
+            if (System.IO.File.Exists(arquivo))
+            {
+                System.IO.StreamReader arqTXT = new System.IO.StreamReader(arquivo);
+                //
+                while ((linha = arqTXT.ReadLine()) != null)
+                {
+                    for (int index = 0; index < linha.Length; index++)
+                    {
+                        caminho += linha[index];
+                    }
+                }
+                //
+                arqTXT.Close();
+            }
+            else
+            {
+                caminho = string.Empty;
+            }
+            //
+            return caminho.Trim();
+
+            #endregion
+        }
+
+        public int GetTempo()
+        {
+            #region SEGUNDOS txt
+
+            string arquivo = AppDomain.CurrentDomain.BaseDirectory + @"\CONFIG\TEMPO.txt";
+            string linha;
+            int tempo = 0;
+            string aux = string.Empty;
+            //
+            if (System.IO.File.Exists(arquivo))
+            {
+                System.IO.StreamReader arqTXT = new System.IO.StreamReader(arquivo);
+                //
+                while ((linha = arqTXT.ReadLine()) != null)
+                {
+                    for (int index = 0; index < linha.Length; index++)
+                    {
+                        aux += linha[index];
+                    }
+                }
+                //
+                arqTXT.Close();
+            }
+
+            tempo = string.IsNullOrEmpty(aux) ? tempo : int.Parse(aux.Trim());
+            //
+            return tempo;
+
+            #endregion
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            lbAviso.Visible = false;
+            timerCount.Start();
+            statusConexao = 1;
+        }
+
+        private void timerCount_Tick(object sender, EventArgs e)
+        {
+            timerCount.Stop();
+            timerCount.Start();
+            //
+            statusConexao = 1;
+            //
+            if (statusConexao == 1)
+            {
+                lbtime.Text = segundos.ToString() + ":s";
+                segundos = segundos + 1;
+                //
+                if (segundos == tempo)//5 segundos
+                {
+                    timerCount.Stop();
+                    timerCount.Start();
+                    //
+                    GetArquivo();
+                    //
+                    segundos = 0;
+                }
+            }
+            else
+            {
+                lbAviso.Text = "PARADO";
+                lbAviso.Visible = true;
+                lbAviso.ForeColor = System.Drawing.Color.Red;
+                statusConexao = 0;
+                segundos = 0;
+                lbtime.Text = "0:s";
+            }
+        }
+
+        private void btParar_Click(object sender, EventArgs e)
+        {
+            lbAviso.Text = "PARADO";
+            lbAviso.Visible = true;
+            lbAviso.ForeColor = System.Drawing.Color.Red;
+            timerCount.Stop();
+            //
+            lbtime.Text = "0:s";
+            segundos = 0;
+        }
+
+    }
+}
